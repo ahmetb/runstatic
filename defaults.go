3668971ab2d58d0d7cc738257c91bd13cba610ca
Ahmet Alp Balkan <ahmetb@google.com>
2019-12-25 14:59:23 -0800

    higher level methods for dealing with config values
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ defaults.go
@@ -0,0 +1,22 @@
+package main
+
+var (
+	defaultProjectFile = mustDotDir().File("project")
+)
+
+func readConfig(file dotFile) (string, bool, error) {
+	ok, err := file.Exists()
+	if err != nil {
+		return "", false, err
+	}
+	if !ok {
+		return "", false, nil
+	}
+	v, err := file.Read()
+	return v, true, err
+}
+
+func writeConfig(file dotFile, value string) error { return file.Write(value) }
+
+func defaultProject() (string, bool, error) { return readConfig(defaultProjectFile) }
+func setDefaultProject(p string) error      { return writeConfig(defaultProjectFile, p) }
