10be57968c8135cfe078bacb4ac1bbd30041183c
Ahmet Alp Balkan <ahmetb@google.com>
2019-12-25 15:00:00 -0800

    3-legged oauth (3lo)
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ go.mod
@@ -0,0 +1,12 @@
+module runstatic
+
+go 1.13
+
+require (
+	github.com/AlecAivazis/survey/v2 v2.0.5
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.11 // indirect
+	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6
+	google.golang.org/api v0.15.0
+)
---
+++ login.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"github.com/fatih/color"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/cloudresourcemanager/v1"
+	googleoauth2 "google.golang.org/api/oauth2/v2"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+var (
+	credsFile = mustDotDir().File("credentials.json")
+)
+
+var (
+	googel = color.HiBlueString("G") +
+		color.HiRedString("o") +
+		color.YellowString("o") +
+		color.HiBlueString("g") +
+		color.HiGreenString("l") +
+		color.HiRedString("e")
+	gcp = googel + color.HiWhiteString(" Cloud")
+
+	// from Google Developer Console (https://console.developers.google.com).
+	googleOAuth2 = &oauth2.Config{
+		ClientID:     "233694408259-7n6aarqdm3e0aafrtrbu8r9duv46mtsl.apps.googleusercontent.com",
+		ClientSecret: "kpLBlEZT3sxg2-71iptx5WE1",
+		RedirectURL:  "urn:ietf:wg:oauth:2.0:oob",
+		Scopes:       scopes,
+		Endpoint:     google.Endpoint,
+	}
+
+	scopes = []string{
+		googleoauth2.UserinfoEmailScope, // required to read email
+		cloudresourcemanager.CloudPlatformScope, // required for everything GCP
+		"https://www.googleapis.com/auth/cloudplatformprojects", // required to create projects, probably superfluous after cloud-platform scope.
+	}
+)
+
+func authenticate() (*oauth2.Token, error) {
+	url := googleOAuth2.AuthCodeURL("state")
+
+	fmt.Println("runstatic uses your " + googel + " account to deploy.")
+	fmt.Println(color.HiBlackString("Your credentials are not collected, and don't leave your machine."))
+	fmt.Println("Let's authenticate to your " + googel + " account:")
+	urlLabel := color.New(color.FgHiBlack, color.Underline).Sprint(url)
+	open, err := openBrowser(url)
+	if err == nil && open {
+		fmt.Println(
+			"  1. I just opened a browser for you, authorize there.\n" +
+				"     (If the browser window did not come up, try this URL:\n" +
+				"     " + urlLabel + " ).")
+	} else {
+		fmt.Println(
+			"  1. Visit this URL and authorize the application:\n" +
+				"     " + urlLabel)
+	}
+	fmt.Printf("\n" +
+		"  2. " + color.New(color.Bold).Sprint("Copy") + " the code from browser, " +
+		color.New(color.Bold).Sprint("paste here") + ": ")
+
+	in, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		return nil, fmt.Errorf("error reading input: %w", err)
+	}
+	tok, err := googleOAuth2.Exchange(context.TODO(), in)
+	if err != nil {
+		return nil, fmt.Errorf("error exchanging code for token: %w", err)
+	}
+	return tok, nil
+}
+
+func openBrowser(url string) (bool, error) {
+	switch runtime.GOOS {
+	case "linux":
+		return true, exec.Command("xdg-open", url).Start()
+	case "windows":
+		return true, exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		return true, exec.Command("open", url).Start()
+	default:
+		return false, nil
+	}
+}
